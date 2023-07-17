5e3cbb0cb9a79e7cb43f65b5e0bced8c84a8390b
mrtcnycll <mrtcnycl@gmail.com>
2023-07-17 21:01:37 +0300

    Proje dosyası ekle.

---
+++ denemee/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace denemee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 0;
+            Console.WriteLine("Please enter first number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please select an operation (+,-,*,/)");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine(result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine(result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine(result);
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    Console.WriteLine(result);
+                    break;
+                default:
+                    Console.WriteLine("That is not a valid option!");
+                    break;
+
+
+
+            }
+
+
+        }
+    }
+}
